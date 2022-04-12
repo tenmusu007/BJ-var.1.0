ca99774f6e544ab0d8460df71bb6bb3818dcdcd5
Atsuya Tanaka <tenmusu007@gmail.com>
2022-04-12 16:20:54 -0700

    ace function

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 "use strict"
 let cards = []
 let dcards = []
+let ace
 let type = ["c", "d", "h", "s"]
 let hands = cards.length
 let psum = 0
 let dsum = 0
 let number = 0
+let newpsum = 0
 let hasBlackJack = false
 let drawCard = false
 let nodrawCard = true
@@ -38,6 +40,7 @@ function againRule() {
 function startCard() {
     let randomType = Math.floor(Math.random() * 4);
     let randomType2 = Math.floor(Math.random() * 4);
+    console.log(typeof(randomType2))
     let kindOftype = type[randomType];
     let kindOftype2 = type[randomType2];
     picEL[2].classList.add('none')
@@ -62,9 +65,9 @@ function startCard() {
 }
 // number will be confirmed more than 10 or not
 function getNewcard(cardNumber) {
-    if(cardNumber === 1){
+    if (cardNumber === 1) {
         return 11
-    }else if (cardNumber >= 10) {
+    } else if (cardNumber >= 10) {
         return 10
     } else if (cardNumber <= 10 && cardNumber >= 2) {
         return cardNumber
@@ -75,24 +78,25 @@ function getRandomCard() {
     return randomNumber
 }
 function renderGame() {
-    psumEl.textContent = "Sum: " + psum
+    atsu()
+    psumEl.textContent = "Sum: " + newpsum
     goresult()
 }
 function goresult() {
     hasBlackJack = false
     drawCard = false
     nodrawCard = true
-    if (psum === 21) {
+    if (newpsum === 21) {
         hasBlackJack = true
         message = "You've got Black Jack"
         // resultEl.textContent = message
-    } else if (psum > 21) {
+    } else if (newpsum > 21) {
         nodrawCard = false
         message = "You Bust"
         resultEl.textContent = message
         standEl.classList.add('none')
         hitEl.classList.add('none')
-    } else if (psum < 21) {
+    } else if (newpsum < 21) {
         drawCard = true
     }
     resultEl.textContent = message
@@ -103,11 +107,23 @@ function newCard() {
         let card1 = getNewcard(card)
         psum += card1
         cards.push(card1)
+        // ace =cards.includes(11)
+        // atsu()
         insertPic(card)
     } else if (nodrawCard === true && drawCard === true) {
         message = "Click start again"
     }
     resultEl.textContent = message
+}
+console.log(ace)
+const atsu = () => {
+    if (cards.includes(11)) {
+        if (psum > 21) {
+            newpsum = psum - 10
+        }
+    }else{
+        newpsum = psum
+    }
 }
 // card arrey からとってきてるから写真と数字が連動してる
 function insertPic(num) {
@@ -161,11 +177,11 @@ function skipCard() {
             insertPic1(dcard)
             // dsumEl.textContent = "Sum: " + dsum
             if (dsum >= 17 && dsum <= 21) {
-                if (dsum === psum) {
+                if (dsum === newpsum) {
                     tie = true
-                } else if (dsum < psum) {
+                } else if (dsum < newpsum) {
                     dealerdrawCard = false
-                } else if (dsum > psum) {
+                } else if (dsum > newpsum) {
                     lose1 = true
                 }
             } else if (dsum > 21) {
@@ -174,9 +190,9 @@ function skipCard() {
         }
 
     } else if (dsum > 17) {
-        if (dsum < psum) {
+        if (dsum < newpsum) {
             dealerdrawCard = false
-        } else if (dsum > psum) {
+        } else if (dsum > newpsum) {
             lose1 = false
         }
     }
